2ce30b2cb411dad6599f58096d97adc719c99568
RobWood8426 <robwoo8426@gmail.com>
2016-01-17 18:02:14 +0200

    Random Map generating

--- Main.py
+++ Main.py
@@ -1,32 +1,23 @@
 import pygame
 from pygame.locals import *
-
+import random
+displaySize = [1600, 1000]
 white = [255, 255, 255]
-displaySize = [1200, 750]
-lineSize = 3
-start = [600, 375]
-end = [650, 425]
+
+gameMap = [[random.randint(0,1) for i in range(16)] for j in range(10)]
+height = 100
+width = 160
 
 pygame.init()
+screen = pygame.display.set_mode(displaySize, pygame.FULLSCREEN)
 
-screen = pygame.display.set_mode(displaySize)
+def displayMap(gameMap):
+    for y in range(10):
+        for x in range(16):
+            if gameMap[y][x] == 1:
+                pygame.draw.line(screen,white,(x*100,(0.5+y)*100),((x+1)*100,(0.5+y)*100),100)
+    pygame.display.update()
+displayMap(gameMap)
 
-
-
-paused = False
-
-while True:
-    while not paused:
-
-        pygame.draw.line(screen, white, start, end, lineSize)
-        pygame.display.update()
-
-        start = end
-
-        end[0] = end[0]+50
-        end[1] = end[1]+50
-
-
-
-    if paused:
-        'Show Menu'
+pygame.time.wait(5000)
+pygame.quit()
